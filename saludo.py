ce7959efd52d05fcd74ca0a843836833feab1d24
Alumno <diegocode@gmail.com>
2016-06-01 18:25:46 -0300

    Saluda al usuario con bienvenida

--- saludo.py
+++ saludo.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
 # Un simple programa de "Hola mundo!"
+# pero que le pide el nombre al usuario y lo saluda.
 
-print("Hola mundo!")
+print("Bienvenido!!")
+print("------------")
+
+nombre = input("Cómo te llamás? ")
+
+print("Hola {}!".format(nombre))
